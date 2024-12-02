bdf1ec5a5cf2ffb6ea7c46327cf9d4e0340d0875
jdkandersson <david.andersson@canonical.com>
2024-12-02 16:15:16 +1100

    change to list

--- src/paas_charm_cli/deploy.py
+++ src/paas_charm_cli/deploy.py
@@ -6,7 +6,7 @@ import subprocess
 def deploy() -> None:
     """Deploy the PaaS Charm application."""
     print("creating rock")
-    subprocess.check_output("rockcraft pack")
+    subprocess.check_output(["rockcraft", "pack"])
     print("creating charm")
-    subprocess.check_output("cd charm && charmcraft pack")
+    subprocess.check_output(["cd", "charm", "&&", "charmcraft", "pack"])
     print("deployed application")
