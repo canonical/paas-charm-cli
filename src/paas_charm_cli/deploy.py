6724e9adafe45b045cb638b0f9e571238a96f48b
jdkandersson <david.andersson@canonical.com>
2024-12-02 17:44:24 +1100

    print stdout

--- src/paas_charm_cli/deploy.py
+++ src/paas_charm_cli/deploy.py
@@ -19,10 +19,11 @@ def deploy() -> None:
     rockcraft_pack_out = subprocess.check_output(
         ["rockcraft", "pack"], stderr=subprocess.STDOUT
     ).decode(encoding="utf-8")
+    print(rockcraft_pack_out)
     rock_name = re.search(
         "^Packed (.+\\.rock)", rockcraft_pack_out, re.MULTILINE
     ).group(1)
-    subprocess.check_output(
+    skopeo_copy_out = subprocess.check_output(
         [
             "rockcraft.skopeo",
             "--insecure-policy",
@@ -31,9 +32,14 @@ def deploy() -> None:
             f"oci-archive:{rock_name}",
             f"docker://{deploy_variables['image_registry']}/"
             f"{rock_info['name']}:{rock_info['version']}",
-        ]
+        ],
+        stderr=subprocess.STDOUT,
     )
+    print(skopeo_copy_out)
 
     print("creating charm")
-    subprocess.check_output(["charmcraft", "pack"], cwd=pathlib.Path() / "charm")
+    charmcraft_pack_out = subprocess.check_output(
+        ["charmcraft", "pack"], stderr=subprocess.STDOUT, cwd=pathlib.Path() / "charm"
+    )
+    print(charmcraft_pack_out)
     print("deployed application")
