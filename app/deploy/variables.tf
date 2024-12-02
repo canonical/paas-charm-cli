2a3e98de030fb9d9b092f5afbc24419a872d37da
jdkandersson <david.andersson@canonical.com>
2024-12-03 01:32:56 +1100

    add deploy step

--- app/deploy/variables.tf
+++ app/deploy/variables.tf
@@ -1,3 +1,7 @@
 variable "image_registry" {
   type = string
 }
+
+variable "model_name" {
+  type = string
+}
--- src/paas_charm_cli/deploy.py
+++ src/paas_charm_cli/deploy.py
@@ -24,6 +24,7 @@ def deploy() -> None:
         "^Packed (.+\\.rock)", rockcraft_pack_out, re.MULTILINE
     ).group(1)
     print("uploading rock to registry")
+    app_image = f"{deploy_variables['image_registry']}/{rock_info['name']}:{rock_info['version']}"
     skopeo_copy_out = subprocess.check_output(
         [
             "rockcraft.skopeo",
@@ -31,17 +32,45 @@ def deploy() -> None:
             "copy",
             "--dest-tls-verify=false",
             f"oci-archive:{rock_name}",
-            f"docker://{deploy_variables['image_registry']}/"
-            f"{rock_info['name']}:{rock_info['version']}",
+            app_image,
         ],
         stderr=subprocess.STDOUT,
     ).decode(encoding="utf-8")
     print(skopeo_copy_out)
 
     print("creating charm")
+    charm_info = yaml.safe_load(
+        (pathlib.Path() / "charm" / "charmcraft.yaml").read_text()
+    )
     charmcraft_pack_out = subprocess.check_output(
         ["charmcraft", "pack"], stderr=subprocess.STDOUT, cwd=pathlib.Path() / "charm"
     ).decode(encoding="utf-8")
     print(charmcraft_pack_out)
+    charm_name = re.search(
+        "^Packed (.+\\.charm)", rockcraft_pack_out, re.MULTILINE
+    ).group(1)
+
+    print("deploying app")
+    juju_add_model_out = subprocess.check_output(
+        ["juju", "add-model", deploy_variables["model_name"]], stderr=subprocess.STDOUT
+    ).decode(encoding="utf-8")
+    print(juju_add_model_out)
+    juju_deploy_model_out = subprocess.check_output(
+        [
+            "juju",
+            "deploy",
+            f"./{charm_name}",
+            charm_info["name"],
+            "--resource",
+            f"flask-app-image={app_image}",
+        ],
+        stderr=subprocess.STDOUT,
+        cwd=pathlib.Path() / "charm",
+    ).decode(encoding="utf-8")
+    print(juju_deploy_model_out)
+    juju_status_out = subprocess.check_output(
+        ["juju", "status"], stderr=subprocess.STDOUT
+    ).decode(encoding="utf-8")
+    print(juju_status_out)
 
     print("deployed application")
