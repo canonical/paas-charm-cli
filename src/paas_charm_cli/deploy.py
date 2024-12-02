b7b8fd5bffcc9d2006d42ff32b5491f21055937c
jdkandersson <david.andersson@canonical.com>
2024-12-02 16:12:04 +1100

    pack rock and charm

--- src/paas_charm_cli/deploy.py
+++ src/paas_charm_cli/deploy.py
@@ -1,6 +1,12 @@
 """Deploy the PaaS Charm application."""
 
+import subprocess
+
 
 def deploy() -> None:
     """Deploy the PaaS Charm application."""
+    print("creating rock")
+    subprocess.check_output("rockcraft pack")
+    print("creating charm")
+    subprocess.check_output("cd charm && charmcraft pack")
     print("deployed application")
